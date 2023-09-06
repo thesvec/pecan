c085d72cc0484c2ca2d8fae5bafa2ea7ddf58c22
Sebastian Svec <sebastian.svec@atlas.cz>
2023-09-06 22:55:00 +0200

    improved the cli and added autodeletion of temp files

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,70 @@ pub use parser::*;
 mod codegen;
 pub use codegen::*;
 
+fn print_help(this: &str) {
+  println!(
+    "Usage: {} <run option> [options] <file>
+
+Run options:
+  -c <file>       Compile to x86_64 linux executable
+  -i <file>       Interpret program
+  -h, --help      Print this help message
+
+Options:
+  -o <out file>   Output file name",
+    this
+  );
+}
+
 fn main() -> ExitCode {
   let args = std::env::args().collect::<Vec<String>>();
 
+  let mut bin_file = "a";
+
   let file = match args.len() {
-    2 => args[1].clone(),
+    1 | 2 => {
+      if args.len() == 2 && (args[1] == "-h" || args[1] == "--help") {
+        print_help(&args[0]);
+        return ExitCode::SUCCESS;
+      } else {
+        eprintln!("Usage: {} <run option> [options] <file>", args[0]);
+        return ExitCode::FAILURE;
+      }
+    },
+    3 => {
+      if args[1] == "-c" {
+        args[2].clone()
+      } else if args[1] == "-i" {
+        eprintln!("Interpret not implemented");
+        return ExitCode::FAILURE;
+      } else {
+        eprintln!("Invalid option: {}", args[1]);
+        return ExitCode::FAILURE;
+      }
+    },
+    5 => {
+      if args[1] == "-c" {
+        if args[2] == "-o" {
+          bin_file = &args[3];
+          args[4].clone()
+        } else {
+          eprintln!("Invalid option: {}", args[3]);
+          return ExitCode::FAILURE;
+        }
+      } else {
+        eprintln!("Invalid option: {}", args[1]);
+        return ExitCode::FAILURE;
+      }
+    },
     _ => {
-      eprintln!("Usage: {} <file>.pn", args[0]);
+      eprintln!("Usage: {} <run option> [options] [file]", args[0]);
       return ExitCode::FAILURE;
     },
   };
+
+  let out_file = &format!("{}.asm", bin_file);
+  let obj_file = &format!("{}.o", bin_file);
+  let bin_file = &format!("{}.out", bin_file);
 
   let input = match std::fs::read_to_string(file) {
     Ok(input) => input,
@@ -67,10 +121,6 @@ fn main() -> ExitCode {
 
   let output = generator.generate();
 
-  let out_file = "out.asm";
-  let obj_file = "out.o";
-  let bin_file = "out";
-
   match std::fs::write(out_file, output) {
     Ok(_) => (),
     Err(err) => {
@@ -103,6 +153,17 @@ fn main() -> ExitCode {
 
   if !status.success() {
     eprintln!("Failed to link");
+    return ExitCode::FAILURE;
+  }
+
+  let status = std::process::Command::new("rm")
+    .arg(out_file)
+    .arg(obj_file)
+    .status()
+    .expect("Failed to execute rm");
+
+  if !status.success() {
+    eprintln!("Failed to remove temporary files");
     return ExitCode::FAILURE;
   }
 
